cf743166eb8c47b2b60a0518f5559222f524196a
floorbear2723 <floorbear2723@gmail.com>
2022-02-21 22:09:54 +0900

    이동원 : 02/21 숙제 완료

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,25 +1,22 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
 
-
-
-
-int main()
+class hi
 {
-    const char* charArray[3] = { "My name is Dongwon" ,"Hellow" ,"32323" };
+public:
+	hi()
+	{}
+	~hi()
+	{
+		printf("으악");
+	}
+};
 
 
-    int pA = 1;
-    int pB = 2;
-    int pC = 3;
 
-    int* intArray[3];
-    intArray[0] = &pA;
-    intArray[1] = &pB;
-    intArray[2] = &pC;
-    
-    
-    int a =sizeof(charArray[0]);
-    int b = sizeof(charArray[1]);
+void main() {
+	hi* good = new hi();
+	delete good;
+	printf("dd");
 
-    return 0;
 }
