67a38d655136a39b0f16d0106b3b94ca87941723
floorbear2723 <floorbear2723@gmail.com>
2022-02-25 04:07:36 +0900

    이동원 : 0224 숙제완료

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,26 +1,22 @@
 ﻿#include <iostream>
 
+int Value = 0;
 
-
-
+int fun()
+{
+	Value++;
+	if (Value < 30)
+	{
+		fun();
+	}
+	Value++;
+	return Value;
+}
 
 int main() {
-	int arry[9] = {14,2,5,29,123,5,9,3,9 };
 
-	for (int i = 0; i < 8; i++) //n-1까지
-	{
-		for (int j = 0; j < (9 - i - 1);j++) //n-1-i
-		{			
-			if (arry[j] > arry[j + 1])
-			{
-				int temp = arry[j + 1];
-				arry[j + 1] = arry[j];
-				arry[j] = temp;
-			}
-		}
-	}
+	int a[30] = fun();
 
-
-	return 0;
+	std::cout << a << std::endl;
 }
 
